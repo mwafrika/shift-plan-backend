c5855cd94216ffcdf6e27621b50e894b0402f092
mwafrika <mwafrikajosue@gmail.com>
2023-10-02 16:18:25 +0300

    Setup docker

---
+++ app.js
@@ -0,0 +1,13 @@
+const app = require("express")();
+
+app.get("/", (req, res) => {
+  res.send({
+    message: "Welcome to shift plan",
+  });
+});
+
+const PORT = 7000;
+
+app.listen(PORT, () => {
+  console.log(`The server is listening on ${PORT}`);
+});
