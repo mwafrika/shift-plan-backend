58f10b3814f0e715c3f13fed8ae16d598b866f91
mwafrika <mwafrikajosue@gmail.com>
2023-10-05 11:44:43 +0300

    Update readme with coverage badge

--- src/utils/manageEnv.js
+++ src/utils/manageEnv.js
@@ -1,13 +1,13 @@
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
+
 dotenv.config();
 const setPort = (environment) => {
-  if (environment === "development") {
+  if (environment === 'development') {
     return process.env.DEV_PORT || 5000;
-  } else if (environment === "test") {
+  } if (environment === 'test') {
     return process.env.TEST_PORT || 5001;
-  } else {
-    return process.env.PORT || 5002;
   }
+  return process.env.PORT || 5002;
 };
 
 export default setPort;
--- test/auth/test.test.js
+++ test/auth/test.test.js
@@ -1,12 +1,9 @@
-// sample test using jest
-// https://jestjs.io/docs/en/getting-started.html
+import request from 'supertest';
+import app from '../../app';
 
-import request from "supertest";
-import app from "../../app";
-
-describe("Test the root path", () => {
-  test("It should response the GET method", async () => {
-    const response = await request(app).get("/api/v1");
+describe('Test the root path', () => {
+  test('It should response the GET method', async () => {
+    const response = await request(app).get('/api/v1');
     expect(response.statusCode).toBe(200);
   });
 });
