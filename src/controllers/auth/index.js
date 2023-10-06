9b80e6fd29b9c488efa1d820e35f328a27f8a767
jefftrojan <j.dauda@alustudent.com>
2023-10-06 15:09:46 +0200

    controllers/auth

--- src/controllers/auth/index.js
+++ src/controllers/auth/index.js
@@ -13,6 +13,62 @@ import {
   sendEmail,
 } from "../../utils/auth";
 
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await findUserByEmail(email);
+  if (!user) {
+    return res.status(404).send({
+      message: 'User not found',
+    });
+  }
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+  if (!isPasswordValid) {
+    return res.status(401).send({
+      message: 'Invalid credentials',
+    });
+  }
+  const token = generateToken(user);
+  return res.status(StatusCodes.OK).send({
+    message: 'Login successful',
+    token,
+  });
+};
+export const register = async (req, res) => {
+  const { email, password, name } = req.body;
+  try {
+    const user = await findUserByEmail(email);
+    if (user) {
+      return res.status(409).send({
+        message: 'User already exists',
+      });
+    }
+    const hashedPassword = await hashPassword(password);
+    const newUser = await createUser({ email, password: hashedPassword, name });
+    if (!newUser) {
+      return res.status(400).send({
+        message: 'Error while creating user',
+      });
+    }
+    const responseData = {
+      id: newUser.id,
+      email: newUser.email,
+      name: newUser.name,
+      createdAt: newUser.createdAt,
+      updatedAt: newUser.updatedAt,
+    };
+    const generatedToken = generateToken(newUser);
+    return res.status(StatusCodes.CREATED).send({
+      message: 'User created successfully',
+      token: generatedToken,
+      user: responseData,
+    });
+  } catch (error) {
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send({
+      message: error.message,
+    });
+  }
+};
+
 export const forgetPassword = async (req, res) => {
   const { email } = req.body;
 
