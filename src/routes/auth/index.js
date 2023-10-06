0c5a175c48fd62885f16c1589913b670899026f8
ciraganenicole <ciraganenicole@gmail.com>
2023-10-06 13:04:54 -0700

    Add routes

---
+++ src/routes/auth/index.js
@@ -0,0 +1,13 @@
+import { Router } from 'express';
+import {
+  login,
+  register,
+  forgetPassword,
+  resetPassword,
+} from '../../controllers/auth';
+const router = Router()
+  .post('/login', login)
+  .post('/register', register)
+  .post('/forget-password', forgetPassword)
+  .post('/reset-password/:id/:token', resetPassword);
+export default router;
