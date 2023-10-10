6cad681d0b0f07a9c74ffab4d15fc79f1f0c6570
mwafrika <mwafrikajosue@gmail.com>
2023-10-10 20:49:47 +0300

    Create user and company

--- src/database/models/index.js
+++ src/database/models/index.js
@@ -28,10 +28,10 @@ fs.readdirSync(__dirname,)
       && file.slice(-3,) === ".js"
       && file.indexOf(".test.js",) === -1,
   )
-  .forEach((file,) => {
-    const model = require(path.join(__dirname, file,),)(
+  .forEach((file) => {
+    const model = require(path.join(__dirname, file))(
       sequelize,
-      Sequelize.DataTypes,
+      Sequelize.DataTypes
     );
     db[model.name] = model;
   },);
