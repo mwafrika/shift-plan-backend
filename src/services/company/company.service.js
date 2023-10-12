efdaf76048baa06972cd12b32d92a64fa8544060
ciraganenicole <ciraganenicole@gmail.com>
2023-10-12 05:47:11 -0700

    Add read and delete company features

--- src/services/company/company.service.js
+++ src/services/company/company.service.js
@@ -9,3 +9,18 @@ export const findCompanyById = async (id, options = {}) => Company.findOne({
 
 export const createCompany = async (company) => Company.create(company);
 export const getCompanyWhere = async (where) => Company.findOne({ where });
+
+export const findCompanyByPK = async (id, options = {}) => Company.findByPk(id, options);
+
+export const findCompanyByName = async (name, options = {}) => Company.findOne({
+  where: {
+    name,
+  },
+  ...options,
+});
+
+export const deleteCompany = async (id) => Company.destroy({
+  where: {
+    id,
+  },
+});
